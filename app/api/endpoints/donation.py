670730ee595015537790291a58f755b5ccb778e0
Stanislav Berezovskii <berezovskii.stas@gmail.com>
2023-06-27 10:59:18 +0200

    SPIRNT22 1st fix iter

--- app/api/endpoints/donation.py
+++ app/api/endpoints/donation.py
@@ -1,11 +1,13 @@
+from fastapi import APIRouter, Depends
+from sqlalchemy.ext.asyncio import AsyncSession
+
 from app.core.db import get_async_session
 from app.core.user import current_superuser, current_user
 from app.crud.donation import donation_crud
 from app.models import User
 from app.schemas.donation import DonationAllDB, DonationCreate, DonationDB
 from app.services.distribution_investments import investment_process
-from fastapi import APIRouter, Depends
-from sqlalchemy.ext.asyncio import AsyncSession
+
 
 router = APIRouter()
 
--- app/api/validators.py
+++ app/api/validators.py
@@ -1,8 +1,9 @@
+from fastapi import HTTPException
+from sqlalchemy.ext.asyncio import AsyncSession
+
 from app.crud.charity_project import charity_project_crud
 from app.models.charity_project import CharityProject
 from app.schemas.charity_project import CharityProjectUpdate
-from fastapi import HTTPException
-from sqlalchemy.ext.asyncio import AsyncSession
 
 
 async def check_name_duplicate(
